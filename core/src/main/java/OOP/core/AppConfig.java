86f784c5522106cd76dfee9ee12f380b6525f6b8
unknown <gvf156@gmail.com>
2021-07-29 16:31:59 +0900

    Refactor Appconfig.

--- core/src/main/java/OOP/core/AppConfig.java
+++ core/src/main/java/OOP/core/AppConfig.java
@@ -1,6 +1,8 @@
 package OOP.core;
 
+import OOP.core.discount.DiscountPolicy;
 import OOP.core.discount.FixDiscountPolicy;
+import OOP.core.member.MemberRepository;
 import OOP.core.member.MemberService;
 import OOP.core.member.MemberServiceImpl;
 import OOP.core.member.MemoryMemberRepository;
@@ -10,14 +12,21 @@ import OOP.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 }
