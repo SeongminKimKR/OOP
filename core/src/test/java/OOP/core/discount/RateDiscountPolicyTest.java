dec87e53e279c49cbfd6bb18000aad69167aff62
unknown <gvf156@gmail.com>
2021-07-29 14:57:23 +0900

    Develop new Discount policy and test this.

---
+++ core/src/test/java/OOP/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,34 @@
+package OOP.core.discount;
+
+import OOP.core.member.Grade;
+import OOP.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        int discount =discountPolicy.discount(member, 10000);
+
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x() {
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+
+        int discount =discountPolicy.discount(member, 10000);
+
+        assertThat(discount).isEqualTo(0);
+    }
+}
