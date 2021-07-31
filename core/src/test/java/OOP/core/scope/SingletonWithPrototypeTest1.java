0eb531fc02ecece27a0ba0d450eec4c4fcfca57d
unknown <gvf156@gmail.com>
2021-07-31 23:04:45 +0900

    Test singletonClientUsePrototype by JSR-330.

--- core/src/test/java/OOP/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/OOP/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,15 @@ package OOP.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,23 +38,32 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     //Default라 안해도 됨
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
-        //1개라 없어도 됨
+        /*@Autowired
+        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;*/
+
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> provider;
+
+        /**
+         * 생성자 주입
+         */
+        /*@Autowired
+        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
+        }*/
 
         public int logic() {
+            //PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = provider.get();
             prototypeBean.addCount();
-            ;
             return prototypeBean.getCount();
         }
     }
